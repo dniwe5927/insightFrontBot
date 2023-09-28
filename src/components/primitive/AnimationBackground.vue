6528e31c8677f752c56f70c814770965abe98096
blameBit <blameBit>
2023-09-28 06:13:58 +0000

    first

---
+++ src/components/primitive/AnimationBackground.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="animated-background">
+    <html>
+      <head></head>
+      <body></body>
+    </html>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@mixin dots($count) {
+  $text-shadow: ();
+  @for $i from 0 through $count {
+    $text-shadow: $text-shadow,
+                 (-.5+(random()) * 2) + em
+                 (-.5+(random()) * 3) + em
+                 7px
+                 #00B4D8;
+  }
+  text-shadow: $text-shadow;
+}
+
+html {
+  font: 5vmin/1.3 Serif;
+  overflow: hidden;
+  background: #007FFF;
+}
+
+body, head {
+  display: block;
+  font-size: 52px;
+  color: transparent;
+}
+
+head::before, head::after,
+body::before, body::after {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 3em;
+  height: 3em;
+  content: '.';
+  mix-blend-mode: screen;
+  animation: 44s -27s move infinite ease-in-out alternate;
+}
+
+
+body::before {
+  @include dots(40);
+  animation-duration: 4s;
+  animation-delay: -27s;
+}
+
+body::after {
+  @include dots(40);
+  animation-duration: 3s;
+  animation-delay: -32s;
+}
+
+head::before {
+  @include dots(40);
+  animation-duration: 2s;
+  animation-delay: -23s;
+}
+
+head::after {
+  @include dots(40);
+  animation-duration: 1s;
+  animation-delay: -19s;
+}
+
+
+@keyframes move {
+  from {
+    transform: rotate(0deg) scale(70) translateX(-20px);
+  }
+  to {
+    transform: rotate(360deg) scale(70) translateX(20px);
+  }
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import WebApp from '@twa-dev/sdk'
+import eruda from 'eruda'
+
+
+eruda.init();
+WebApp.ready();
+
+createApp(App).mount('#app')
